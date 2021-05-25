91f36e039aa6a31c41b1e0f1ded46bc296385c79
vitor <vitordacruz@gmail.com>
2021-05-24 22:42:36 -0300

    Feign client

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -1,33 +1,22 @@
 package com.devsuperior.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.devsuperior.hrpayroll.entities.Payment;
 import com.devsuperior.hrpayroll.entities.Worker;
+import com.devsuperior.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 
-	public Payment getPayment(long workedId, int days)	{
-		
-		Map<String , String> uriVariables = new HashMap<>();
-		
-		uriVariables.put("id", "" + workedId);
-		
-		ResponseEntity<Worker> response = restTemplate.getForEntity(workerHost + "/workers/{id}", Worker.class, uriVariables);		
+	public Payment getPayment(long workerId, int days)	{
+
+		ResponseEntity<Worker> response = workerFeignClient.findById(workerId);
 		
 		Worker worker = response.getBody();
 		
