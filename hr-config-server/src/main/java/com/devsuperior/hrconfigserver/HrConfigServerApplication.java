c2525a8c3f98b54217ec345bffbdd0aa5c1a68cd
vitor <vitordacruz@gmail.com>
2021-05-27 00:28:08 -0300

    Removing unnecessary code in the HrConfigServerApplication.class

--- hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
@@ -1,22 +1,15 @@
 package com.devsuperior.hrconfigserver;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class HrConfigServerApplication implements CommandLineRunner {
+public class HrConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-		
 	}
 
 }
