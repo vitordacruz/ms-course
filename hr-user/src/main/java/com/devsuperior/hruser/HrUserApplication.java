a33cf084c85fc05046427f03e58d809532282a72
vitor <vitordacruz@gmail.com>
2021-05-27 21:44:14 -0300

    Database seeeding

--- hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 @SpringBootApplication
 public class HrUserApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
 	}
